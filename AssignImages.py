4576c85d6c6b83279789970249f99211af3aceec
Timothy Lou <70876288+TimatoPaste@users.noreply.github.com>
2026-02-18 00:09:43 -0800

    Add files via upload

---
+++ AssignImages.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+import pandas as pd
+from PIL import Image 
+from pillow_heif import register_heif_opener
+from pyproj import Transformer
+import pyproj
+
+class Img:
+    name = ""
+    number = -1
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
+
+def get_number(img):
+    """
+    in:
+        -img: Img obj
+    out:
+        -number: number of that Img obj
+    """
+    return img.number
+
+def get_img_names(img_dir):
+    """
+    in:
+        -img_dir: directory of imgs
+    out:
+        -img_name_list: list of file names of imgs in img directory
+    """
+    img_obj_list = []
+    img_name_list = []
+    for f in Path(img_dir).iterdir():
+        if f.is_file():
+            img_obj_list.append(Img(f.name, f.name[-9:-5]))
+    img_obj_list.sort(key=get_number)
+    for img in img_obj_list:
+        img_name_list.append(img.name)
+    return img_name_list
+
+def convert_imgs_to_jpg(src_dir, output_dir):
+    """
+    in:
+        -src_dir: dir to original img folder
+        -output_dir: dir to output
+    out:
+        -folder of converted imgs
+    """
+    register_heif_opener()
+    img_names = get_img_names(src_dir)
+    for img_name in img_names:
+        pil_img = Image.open(f"{src_dir}/{img_name}")
+        if pil_img.mode in ('RGBA', 'P', 'LA'):
+            temp = Image.new("RGB", pil_img.size, (255, 255, 255))
+            temp.paste(pil_img, mask=pil_img.split()[3] if pil_img.mode == 'RGBA' else None)
+            pil_img = temp
+        pil_img.save(f"./{output_dir}/{img_name[:-4]}jpg", 'JPEG', quality=95)
+
+def assign_images(img_dir, points_dir, output_dir):
+    """
+    in:
+        -img_dir: dir to img folder
+        -points_dir: dir to points csv
+        -output_dir: dir to output
+    out:
+        -saved point.colmap formatted file
+    """
+    pyproj.network.set_network_enabled(True)
+    transformer = Transformer.from_crs("EPSG:4979", "EPSG:4326+5773", always_xy=True)
+    with open(output_dir, 'w') as file:
+        img_name_list = get_img_names(img_dir)
+        points = pd.read_csv(points_dir)
+        for idx, row in points.iterrows():
+            ellipsoidal_height = float(row['Ellipsoidal height'])
+            latitude = float(row['Latitude'])
+            longitude = float(row['Longitude'])
+            _, _, altitude = transformer.transform(longitude, latitude, ellipsoidal_height)
+            file.write(f"{img_name_list[idx]} {latitude} {longitude} {altitude}\n")
+# convert_imgs_to_jpg("./imgs", "./jpg_imgs")
+assign_images("./chem_jpg_imgs", "./chem.csv", "./chem.txt")
---
+++ colmap2ply.py
@@ -0,0 +1,85 @@
+import pycolmap
+import pymap3d as pm
+from pathlib import Path
+
+def get_recons(proj_dir):
+    """
+    in:
+        -proj_dir: directory where the map folders are (like the parent dir of the 0, 1, 2 recons. If there's only one recon, then just make an obj)
+    out:
+        -recon_list: list of recons 
+    """
+    proj_path = Path(proj_dir)
+    folder_list = [folder for folder in proj_path.iterdir() if folder.is_dir()]
+    recon_list = [pycolmap.Reconstruction(f"{proj_dir}/{folder.name}") for folder in folder_list]
+    return recon_list
+
+def get_point3Ds(recon):
+    """
+    in:
+        -recon: recon obj
+    out:
+        -point3D_list: list of point3D objs 
+    """
+    point3D_list = [recon.point3D(id) for id in recon.point3D_ids()]
+    return point3D_list
+
+def get_xyzs(recon):
+    """
+    in:
+        -recon: recon obj
+    out:
+        -point3D_xyz_set: set of 3d tuples with xyz of every point in recon obj (typically in ecef after model aligning)
+    """
+    point3D_xyz_set = set()
+    for point in get_point3Ds(recon):
+        point3D_xyz_set.add(tuple(point.xyz))
+    return point3D_xyz_set
+
+def ecefs_to_enus(ecef_list, ref):
+    """
+    in:
+        -ecef_list: list of ecef tuples
+        -ref: lat/lon/alt tuple for reference
+    out:
+        -enu_list: list of enu tuples
+    """
+    enu_list = [pm.ecef2enu(point[0], point[1], point[2], ref[0], ref[1], ref[2]) for point in ecef_list]
+    return enu_list
+
+def make_ply(outputdir, pt_list, comment="none"):
+    """
+    in:
+        -outputdir: dir for output ply including ply file name and its extension
+        -pt_list: list of 3d tuples
+    out:
+        ply file will be in outputdir
+    """
+    with open(outputdir, 'w') as file:
+        file.write("ply\n")
+        file.write("format ascii 1.0\n")
+        file.write(f"comment {comment}\n")
+        file.write(f"element vertex {len(pt_list)}\n")
+        file.write("property float x\n")
+        file.write("property float y\n")
+        file.write("property float z\n")
+        file.write("end_header\n")
+        for pt in pt_list:
+            file.write(f"{pt[0]} {pt[1]} {pt[2]}\n")
+
+#REPLACE THIS WITH THE LAT/LON/ALT THAT YOU WANT TO BE THE ORIGIN. THIS IS SOME POINT ON CAMPBELL
+LAT_0 = 34.41622191
+LON_0 = -119.8456223
+H_0 = 15.223058115078384
+
+
+#example; change the directories, obviously
+recons = get_recons("./chem2")
+chem_recons = get_recons("./chem2")
+
+all_xyzs = set()
+for recon in chem_recons:
+    all_xyzs.update(get_xyzs(recon))
+all_enus = ecefs_to_enus(all_xyzs, (LAT_0, LON_0, H_0))
+make_ply("./plys/chem2.ply", all_enus, "chem2")
+
