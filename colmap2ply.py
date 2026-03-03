5ad72fffc3f76b4260c91f308489bc4cff67c91a
Timothy Lou <70876288+TimatoPaste@users.noreply.github.com>
2026-03-03 13:41:34 -0800

    Update colmap2ply.py

--- colmap2ply.py
+++ colmap2ply.py
@@ -1,20 +1,18 @@
 import pycolmap
-import pymap3d as pm
 from pathlib import Path
+from scipy.spatial.transform import Rotation
 
-def get_recons(proj_dir):
+def get_recons(proj_path):
     """
     in:
-        -proj_dir: directory where the map folders are (like the parent dir of the 0, 1, 2 recons. If there's only one recon, then just make an obj)
+        -proj_path: directory where the map folders are (like the parent dir of the 0, 1, 2 recons. If there's only one recon, then just make an obj)
     out:
         -recon_list: list of recons 
     """
-    proj_path = Path(proj_dir)
-    folder_list = [folder for folder in proj_path.iterdir() if folder.is_dir()]
-    print(folder_list)
-    recon_list = [pycolmap.Reconstruction(f"{proj_dir}/{folder.name}") for folder in folder_list]
-    print(recon_list)
+    folder_list = [folder for folder in Path(proj_path).iterdir() if folder.is_dir()]
+    recon_list = [pycolmap.Reconstruction(f"{proj_path}/{folder.name}") for folder in folder_list]
     return recon_list
+
 
 def get_point3Ds(recon):
     """
@@ -23,13 +21,31 @@ def get_point3Ds(recon):
     out:
         -point3D_list: list of point3D objs 
     """
-    point3D_list = [recon.point3D(id) for id in recon.point3D_ids()]
+    point3D_list = [point3D for id, point3D in recon.points3D.items()]
     return point3D_list
 
-def point3Ds_to_enus(point3D_list, ref):
+def get_img_world_coords(recon):
     """
     in:
-        -point3D_list: list of point3D objs
+        -recon: recon obj
+    out:
+        -output: list of tuples (str img name, img world coords)
+    """
+    output = []
+    for _, image in recon.images.items():
+        if not image.has_pose:
+            continue
+        pose = image.cam_from_world()
+        rotation = pose.rotation.matrix()
+        translation = pose.translation
+        camera_center_world = rotation.T @ -translation
+        output.append((image.name, camera_center_world))
+    return output
+
+def ecefs_to_enus(point3D_list, ref):
+    """
+    in:
+        -point3D_list: list of point3D objs in ecef
         -ref: lat/lon/alt tuple for reference
     out:
         -enu_list: list of enu tuples
@@ -37,16 +53,16 @@ def point3Ds_to_enus(point3D_list, ref):
     enu_list = [pm.ecef2enu(point3D.xyz[0], point3D.xyz[1], point3D.xyz[2], ref[0], ref[1], ref[2]) for point3D in point3D_list]
     return enu_list
 
-def make_ply(outputdir, point3D_list, ref, comment="none"):
+def ply_from_point3D_list(output_path, point3D_list, ref, comment="none"):
     """
     in:
-        -outputdir: dir for output ply including ply file name and its extension
+        -output_path: dir for output ply including ply file name and its extension
         -pt_list: list of point3D objs
         -ref: lat/lon/alt tuple for reference
     out:
-        ply file will be in outputdir
+        ply file will be in output_path
     """
-    with open(outputdir, 'w') as file:
+    with open(output_path, 'w') as file:
         file.write("ply\n")
         file.write("format ascii 1.0\n")
         file.write(f"comment {comment}\n")
@@ -62,19 +78,23 @@ def make_ply(outputdir, point3D_list, ref, comment="none"):
             e, n, u = pm.ecef2enu(point3D.xyz[0], point3D.xyz[1], point3D.xyz[2], ref[0], ref[1], ref[2])
             r, g, b = point3D.color
             file.write(f"{e} {n} {u} {r} {g} {b}\n")
-
 #REPLACE THIS WITH THE LAT/LON/ALT THAT YOU WANT TO BE THE ORIGIN. THIS IS SOME POINT ON CAMPBELL
 LAT_0 = 34.41622191
 LON_0 = -119.8456223
 H_0 = 15.223058115078384
 
 
-#example; change the directories, obviously
-recons = get_recons("./recons/prior/scuffed_campbell_translock_false")
+"""
+example; change the directories, obviously
+
+recons = get_recons("./recons/priors extra options/campbell")
 
 all_point3Ds = []
 for recon in recons:
     all_point3Ds = all_point3Ds + get_point3Ds(recon)
-make_ply("./plys/scuffed_campbellprior.ply", all_point3Ds, (LAT_0, LON_0, H_0), "scuffedcampbellprior")
+ply_from_point3D_list("./plys/priors extra options/campbell.ply", all_point3Ds, (LAT_0, LON_0, H_0), "campbell")
+"""
 
-
+recons = get_recons("./recons/no priors no extra options/chem/no priors no extra options")
+test_recon = recons[0]
+print(get_img_world_coords(test_recon))
